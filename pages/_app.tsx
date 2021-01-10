613607f2d0faf8324485eed9ca79a411cd37cab3
Jose Marques <jjgm98@gmail.com>
2021-01-10 00:30:54 +0000

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,14 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from "@chakra-ui/react"
-import { Box } from '@chakra-ui/react'
 import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider>
             <Head>
-                <title>Insert a task here ✏️</title>
+                <title>Graphic!!!</title>
             </Head>
-            <Box
-                maxW="900px"
-                minH="100vh"
-                mx="auto"
-                shadow="lg"
-                p="2em"
-                d="flex"
-                flexDirection="column"
-                alignItems="center"
-            >
                 <Component {...pageProps} />
-            </Box>
         </ChakraProvider>
     )
 }
